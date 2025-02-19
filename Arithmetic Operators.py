e1e7e4905fe56586def2fc6e2aec1e952f997e72
OĞUZHAN YAZICI <159013707+Oguzhan-printer@users.noreply.github.com>
2025-02-19 08:59:47 +0300

    Update Arithmetic Operators.py

--- Arithmetic Operators.py
+++ Arithmetic Operators.py
@@ -1,3 +1,4 @@
+#Task 1
 
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
@@ -17,3 +18,9 @@ print('Division result:',Division)
 print('Full division result:',Full_division)
 print('Mode retrieval result:',Mode_retrieval)
 print('Base taking result:',Base_taking)
+
+#Task 2
+
+take_radius = int(input("Enter the radius: "))
+area_circle = 3.14 * (take_radius ** 2)
+print(area_circle)
