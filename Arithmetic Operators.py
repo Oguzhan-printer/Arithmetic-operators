b963334c6ea519a0e1629e929590c2cc5739856a
OĞUZHAN YAZICI <159013707+Oguzhan-printer@users.noreply.github.com>
2025-02-19 08:48:52 +0300

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,19 @@
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+Collection = first_number + second_number
+Extraction = first_number - second_number
+Multiplication = first_number * second_number
+Division = first_number / second_number
+Full_division = first_number // second_number
+Mode_retrieval = first_number % second_number
+Base_taking = first_number ** second_number
+
+print('Collection result:',Collection)
+print('Extraction result:',Extraction)
+print('Multiplication result:',Multiplication)
+print('Division result:',Division)
+print('Full division result:',Full_division)
+print('Mode retrieval result:',Mode_retrieval)
+print('Base taking result:',Base_taking)
